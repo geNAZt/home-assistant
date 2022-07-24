fd6d0914f1b7bbb8a37c9ce808abdc603cc4704b
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2022-07-24 20:29:42 +0200

    Ensure that light is out before calibration

--- appdaemon/apps/light.py
+++ appdaemon/apps/light.py
@@ -4,6 +4,10 @@ class Light(hass.Hass):
 
     def initialize(self):
         # We should calibrate the light first
+        self.turn_off(self.args["light"])
+        self.run_in(self.start_calibration, 30)
+
+    def start_calibration(self, kwargs):
         self._beforeCalibrationLux = self.get_state(self.args["luxSensor"])
         self.turn_on(self.args["light"], brightness = 255 / 2)
         self.run_in(self.calibration, 30)
