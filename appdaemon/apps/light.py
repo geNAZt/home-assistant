d7ffb424d78e2497b03261f70020d823bc460b52
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2022-07-24 20:26:24 +0200

    Add seen lights

---
+++ appdaemon/apps/light.py
@@ -0,0 +1,36 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+class Light(hass.Hass):
+
+    def initialize(self):
+        # We should calibrate the light first
+        self._beforeCalibrationLux = self.get_state(self.args["luxSensor"])
+        self.turn_on(self.args["light"], brightness = 255 / 2)
+        self.run_in(self.calibration, 30)
+
+    def calibration(self):
+        self._maxLux = (self.get_state(self.args["luxSensor"]) - self._beforeCalibrationLux) * 2
+        self.log("Detected %d lux" % self._maxLux)
+
+        self.recalc()
+        self.run_every(self.recalc, "now", 60)
+
+    def recalc(self):
+        # Get the actual lux
+        lux = self.get_state(self.args["luxSensor"])
+        neededLux = self.args["wantedLux"] - lux
+
+        if neededLux > 10:
+            neededBrightness = (neededLux / self._maxLux) * 255
+            self.turn_on(self.args["light"], brightness = neededBrightness)
+            self.log("Turned light %s to %d" % (self.args["light"], neededBrightness))
+        elif neededLux < -10:
+            currentBrightness = self.get_state(self.args["light"], attribute="brightness")
+            adjustedBrightness = currentBrightness - ((abs(neededLux) / self._maxLux) * 255)
+            if adjustedBrightness <= 0:
+                self.turn_off(self.args["light"])
+                self.log("Turned light %s off" % self.args["light"])
+            else:
+                self.turn_on(self.args["light"], brightness = adjustedBrightness)
+                self.log("Turned light %s to %d" % (self.args["light"], adjustedBrightness))
+
