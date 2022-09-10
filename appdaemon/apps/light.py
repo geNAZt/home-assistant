c2d4334658c4af0b66856aab6591d063378ae30a
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2022-09-10 11:49:13 +0200

    Add support for light grouping

--- appdaemon/apps/light.py
+++ appdaemon/apps/light.py
@@ -1,70 +1,101 @@
 import appdaemon.plugins.hass.hassapi as hass
+import time
 from simple_pid import PID
 
 class Light(hass.Hass):
 
     def initialize(self):
+        # Setup the PID controller
         self._pid = PID(0.8, 0.0, 0.0, setpoint=float(self.get_state(self.args["wantedLux"])), sample_time=1)
         self._pid.output_limits = (-50, 50)
 
-        # We should calibrate the light first
+        # We should calibrate the light temperature first
         diffLightWarmth = float(self.args["maxLightTemp"]) - float(self.args["minLightTemp"])
         wantedLightWarmth = float(self.args["wantedLightTemp"]) - float(self.args["minLightTemp"])
         self._lightWarmth = (wantedLightWarmth / diffLightWarmth) * 255
 
         # Get presence state
-        self._presence = self.get_state(self.args["presenceSensor"]) == "on"
+        self._presence = self.is_present()
         
-        presenceEntity = self.get_entity(self.args["presenceSensor"])
-        presenceEntity.listen_state(self.onPresenceChange, new = "off")
-        presenceEntity.listen_state(self.onPresenceChange, new = "on")
+        # Attach a listener to all presence sensors
+        for sensor in self.args["presenceSensor"]:
+            presenceEntity = self.get_entity(sensor)
+            presenceEntity.listen_state(self.onPresenceChange, new = "off")
+            presenceEntity.listen_state(self.onPresenceChange, new = "on")
 
-        luxEntity = self.get_entity(self.args["luxSensor"])
-        luxEntity.listen_state(self.onLuxChange)
+        for sensor in self.args["luxSensor"]:
+            luxEntity = self.get_entity(sensor)
+            luxEntity.listen_state(self.onLuxChange)
 
+        # Listen if input changes so we can set setpoint of pid accordingly
         inputEntity = self.get_entity(self.args["wantedLux"])
         inputEntity.listen_state(self.onWantedLuxChange)
 
-        self.turn_on(self.args["light"], brightness = 90, color_temp = self._lightWarmth)
+        # Kick it off
+        self._lastUpdate = 0
+        self.set_light_to(90)
+
+    def set_light_to(self, brightness):
+        for light in self.args["light"]:
+            if brightness == 0:
+                self.turn_off(light)
+            else:
+                self.turn_on(light, brightness = brightness, color_temp = self._lightWarmth)
+
+    def is_present(self):
+        for sensor in self.args["presenceSensor"]:
+            if self.get_state(sensor) == "on":
+                return True
+        
+        return False
 
     def onWantedLuxChange(self, entity, attribute, old, new, kwargs):
         self.log("Wanted lux changed: %r" % new)
         self._pid.setpoint = float(new)
 
     def onLuxChange(self, entity, attribute, old, new, kwargs):
-        self.recalc(kwargs=None)
+        now = time.monotonic()
+        dt = now - self._lastUpdate if (now - self._lastUpdate) else 1e-16
+        if dt > 4:
+            self.recalc(kwargs=None)
+            self._lastUpdate = now
 
     def onPresenceChange(self, entity, attribute, old, new, kwargs):
         if self._presence == False:
-            self._presence = new == "on"
+            self._presence = self.is_present()
             if self._presence:
-                self.turn_on(self.args["light"], brightness = self._restoreValue, color_temp = self._lightWarmth)
+                self.set_light_to(self._restoreValue)
         else: 
-            self._presence = new == "on"
+            self._presence = self.is_present()
             if self._presence == False:
                 # Store old value for restore
                 self._restoreValue = float(self.get_state(self.args["light"], attribute="brightness", default=0))
-                self.turn_off(self.args["light"])
+                self.set_light_to(0)
 
     def recalc(self, kwargs):
         # Check if presence is triggered
         if self._presence == False:
-            self.turn_off(self.args["light"])
+            self.set_light_to(0)
             return
 
         # Get the actual lux
-        lux = float(self.get_state(self.args["luxSensor"]))
+        lux = 0
+        for luxSensor in self.args["luxSensor"]:
+            lux += float(self.get_state(self.args["luxSensor"]))
+
+        lux = lux / len(self.args["luxSensor"])
         power = self._pid(lux)
+        
         self.log("Presence: %r, Lux: %r, Wanted change: %r" % (self._presence, lux, power))
 
         # Calc new brightness
-        currentBrightness = float(self.get_state(self.args["light"], attribute="brightness", default=0))
+        currentBrightness = float(self.get_state(self.args["light"][0], attribute="brightness", default=0))
         adjustedBrightness = currentBrightness + power
 
         # Check what we change
         if adjustedBrightness <= 0:
-            self.turn_off(self.args["light"])
-            self.log("Turned light %s off" % self.args["light"])
+            self.set_light_to(0)
+            self.log("Turned light off")
         else:
-            self.turn_on(self.args["light"], brightness = adjustedBrightness, color_temp = self._lightWarmth)
-            self.log("Turned light %s to %d" % (self.args["light"], adjustedBrightness))
+            self.set_light_to(adjustedBrightness)
+            self.log("Turned light to %d" % (adjustedBrightness))
