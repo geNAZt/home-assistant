3578cd23032f728cebe87e4b3829ae2a7f58ec2e
Fabian Jonassen <fabian.jonassen@paymenttools.com>
2025-01-08 20:47:38 +0100

    Check for non float values

--- appdaemon/apps/heating.py
+++ appdaemon/apps/heating.py
@@ -59,7 +59,11 @@ class Heating(hass.Hass):
             start_time = datetime.datetime.now() - timedelta(minutes = 30)
             data = self.get_history(entity_id = sensor, start_time = start_time)
             for point in data[0]:
-                self.log("> %r: %r" % (point['last_changed'], point['state']))
+                try:
+                    state = float(point['state'])
+                    self.log("> %r: %r" % (point['last_changed'], state))
+                except ValueError:
+                    pass
 
         return float(temperature / len(self.args["roomSensors"]))
     
