11ca43223499304089f0df8c2255b14a35b24dd3
Fabian Jonassen <jonassen.fabian@gmail.com>
2025-01-11 21:31:15 +0100

    Refactor heating and add a cooldown phase

--- appdaemon/apps/heating.py
+++ appdaemon/apps/heating.py
@@ -1,5 +1,6 @@
 import appdaemon.plugins.hass.hassapi as hass
 import datetime
+import time
 
 from datetime import timedelta, datetime, timezone
 
@@ -14,7 +15,8 @@ from datetime import timedelta, datetime, timezone
 
 class Heating(hass.Hass):
 
-    _heating: bool
+    _heating_started: float
+    _heating_halted_until: float
 
     def initialize(self):
         self.log("Heating control loaded...")
@@ -41,6 +43,9 @@ class Heating(hass.Hass):
 
     def target_temp(self):
         return float(self.get_state(self.args["targetTemp"], default=0))
+
+    def is_heating(self):
+        return self.get_state(self.args["output"]) == "on"
 
     # Check if at least one of the security sensors has a temp high enough
     def is_security_shutdown(self):
@@ -113,38 +118,51 @@ class Heating(hass.Hass):
         return temperature
 
     def recalc(self, kwargs):
-        # Check for security shutdown
-        if self.is_security_shutdown():
-            if self._heating:
-              self.log("Turning off heat due to security")
-            self._heating = False
-        else:        
-            room_temp = self.room_temperature()
-            sec_temp = self.security_temperature()
-            sec_temp_rate = self.security_temperature_rate()
-            room_temp_rate = self.room_temperature_rate()
+        heating = self.is_heating()
 
-            self.log("Room temp rate: %r" % room_temp_rate)
-            if room_temp_rate < -0.045:
-                # Open window?
-                self.log("Room has open window. Not heating...")
-                self._heating = False
-            else:
-                # Check if we need to heat
-                diff = self.target_temp() - room_temp
-                if self._heating and room_temp >= self.target_temp():       # We reached target temp
-                    self._heating = False
-                elif self._heating == False and diff > float(self.args["allowedDiff"]):   # The room is too cold we need to heat it
-                    self.log("Room too cold starting to heat. Diff was %r" % diff)
-                    self._heating = True
+        # Check for heating length
+        now_seconds = time.time()
+        if heating and now_seconds - self._heating_started > 900:
+            self._heating_halted_until = now_seconds
+            self._heating_started = 0
+            self.log("Setting heating pause until %r" % self._heating_halted_until)
 
-            diffSecurity = sec_temp - room_temp
-            if self._heating and sec_temp_rate > 0.025:
-                self._heating = False
-                self.log("Wanted to heat but diff between ceiling and floor temp is too high: %r" % diffSecurity)
-
-        switch_state = self.get_state(self.args["output"])
-        if self._heating and switch_state == "off":
-            self.turn_on(self.args["output"])
-        elif self._heating == False and switch_state == "on":
+        # Check if we are paused
+        if self._heating_halted_until > now_seconds:
+            self._heating_halted_until = 0
             self.turn_off(self.args["output"])
+            return
+
+        # Check for security shutdown
+        if heating and self.is_security_shutdown():
+            self.log("Turning off heat due to security")
+            self.turn_off(self.args["output"])
+            return
+
+        # Check for open window (heat leaking)
+        if heating:
+            room_temp_rate = self.room_temperature_rate()
+            if room_temp_rate < -0.045:
+                self.log("Room has open window. Not heating...")
+                self.turn_off(self.args["output"])
+                return
+
+        # Check if diff top to bottom is too strong (heat transfer)
+        if heating and self.security_temperature_rate() > 0.025:
+            self.log("Wanted to heat but diff between ceiling and floor temp is too high")
+            self.turn_off(self.args["output"])
+            return
+
+        room_temp = self.room_temperature()
+
+        # Have we reached target temp?
+        if heating and room_temp >= self.target_temp():       # We reached target temp
+            self.turn_off(self.args["output"])
+            return
+
+        # Do we need to start heating?
+        diff = self.target_temp() - room_temp
+        if heating == False and diff > float(self.args["allowedDiff"]):
+            self._heating_started = now_seconds
+            self.turn_on(self.args["output"])
+            return
