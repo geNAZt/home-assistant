16b077e3f608c4bd840e35f0d1632e4c04b74b65
Fabian Jonassen <fabian.jonassen@paymenttools.com>
2025-02-04 06:56:17 +0100

    Easier lights (for now)

--- appdaemon/apps/light.py
+++ appdaemon/apps/light.py
@@ -1,16 +1,12 @@
 import appdaemon.plugins.hass.hassapi as hass
-import time
 import re
 import math
 
 from tinydb import TinyDB, Query
-from simple_pid import PID
 
 class Light(hass.Hass):
 
     _presence: bool
-    _pid: PID
-    _lastUpdate: int
 
     def initialize(self):
         # Open database
@@ -56,10 +52,6 @@ class Light(hass.Hass):
         })
 
         self.listen_event(self.onEvent, event="call_service")
-
-        # Setup the PID controller
-        self._pid = PID(2.0, 0.5, 2.0, setpoint=float(self.get_state(self.virtual_entity_name, attribute="brightness_pct", default=100)) * 3)
-        self._pid.output_limits = (-10, 40)
     
         # Attach a listener to all presence sensors
         self.presence_sensors = self.find_entity("binary_sensor.presence_%s[_0-9]*" % self.name.replace("light_", ""))
@@ -71,14 +63,6 @@ class Light(hass.Hass):
             presenceEntity.listen_state(self.onPresenceChange, new = "off")
             presenceEntity.listen_state(self.onPresenceChange, new = "on")
 
-        self.lux_sensors = self.find_entity("sensor.lux_%s[_0-9]*" % self.name.replace("light_", ""))
-        if len(self.lux_sensors) == 0:
-            raise Exception("not enough lux sensors")
-
-        for sensor in self.lux_sensors:
-            luxEntity = self.get_entity(sensor)
-            luxEntity.listen_state(self.onLuxChange)
-
         # Get lights
         self.lights = self.find_entity("light.light_[0-9]_%s" % self.name.replace("light_", ""))
         if len(self.lights) == 0:
@@ -86,9 +70,6 @@ class Light(hass.Hass):
         
         # Get presence state
         self._presence = self.is_present()
-
-        # Kick it off
-        self._lastUpdate = 0
 
     def find_entity(self, search, contains_not=""):
         states = self.get_state()
@@ -103,13 +84,6 @@ class Light(hass.Hass):
                     found.append(entity)
 
         return found
-    
-    def state_value(self, name, value):
-        entity_id = "input_number.state_light_%s_%s" % (self.name, name)
-        self.set_state(entity_id, state=value, attributes={
-            "min": -500,
-            "max": 500,
-        })
 
     def convert_K_to_RGB(self, colour_temperature):
         """
@@ -171,10 +145,10 @@ class Light(hass.Hass):
         return red, green, blue
 
     def onEvent(self, event_name, data, kwargs):
-        if not "service_data" in data:
+        if "service_data" not in data:
             return
         
-        if not "entity_id" in data["service_data"]:
+        if "entity_id" not in data["service_data"]:
             return
 
         # This is a nasty hack since it can happen that an array or string is given
@@ -208,12 +182,13 @@ class Light(hass.Hass):
                 self.table.update({'state': 'on', 'color': attr["color_temp_kelvin"]}, doc_ids=[self.db_doc_id])
 
             self.set_state(self.virtual_entity_name, state="on", attributes=attr)
+            
 
         if data["service"] == "turn_off":
             self.table.update({'state': 'off'}, doc_ids=[self.db_doc_id])
             self.set_state(self.virtual_entity_name, state="off")
 
-        self.recalc(kwargs=None)
+        self.update()
 
     def set_light_to(self, brightness):
         self.log("Set light to %r" % brightness)
@@ -234,69 +209,18 @@ class Light(hass.Hass):
         
         return False
 
-    def onLuxChange(self, entity, attribute, old, new, kwargs):
-        # Check if we got disabled
-        active = self.get_state(self.virtual_entity_name)
-        if active == "off":
-            self.set_light_to(0)
-            return
-
-        now = time.monotonic()
-        dt = now - self._lastUpdate if (now - self._lastUpdate) else 1e-16
-        if dt > 4:
-            self.recalc(kwargs=None)
-            self._lastUpdate = now
-
     def onPresenceChange(self, entity, attribute, old, new, kwargs):
+        self.update()
+
+    def update(self):
         # Check if we got disabled
         active = self.get_state(self.virtual_entity_name)
         if active == "off":
             self.set_light_to(0)
             return
 
-        if self._presence == False:
-            self._presence = self.is_present()
-            if self._presence:
-                doc = self.table.get(doc_id=self.db_doc_id)
-                self.set_light_to(doc["restore"])
-        else: 
-            self._presence = self.is_present()
-            if self._presence == False:
-                self.set_light_to(0)
-
-    def recalc(self, kwargs):
-        # Check if we got disabled
-        active = self.get_state(self.virtual_entity_name)
-        if active == "off":
-            self.set_light_to(0)
-            return
-
-        # Check if presence is triggered
-        if not self._presence:
-            self.set_light_to(0)
-            return
-
-        # Get the actual lux
-        lux = 0
-        for luxSensor in self.lux_sensors:
-            lux += float(self.get_state(luxSensor))
-
-        lux = lux / len(self.lux_sensors)
-        power = self._pid(lux)
-
-        # Calc new brightness
-        currentBrightness = self.get_state(self.lights[0], attribute="brightness", default=0)
-        if currentBrightness == None:
-            currentBrightness = 0
-
-        adjustedBrightness = float(currentBrightness) + power
-
-        # Check what we change
-        if adjustedBrightness <= 0:
-            self.set_light_to(0)
+        if self.is_present():
+            self.set_light_to(self.get_state(self.virtual_entity_name, attribute="brightness", default=0))
         else:
-            diff = abs(adjustedBrightness - currentBrightness)
+            self.set_light_to(0)
 
-            if diff > 1:
-                self.table.update({'restore': adjustedBrightness}, doc_ids=[self.db_doc_id])
-                self.set_light_to(adjustedBrightness)
