f0d4aebfa5697b508573c85865d99241e8322afc
Fabian Jonassen <fabian.jonassen@paymenttools.com>
2025-02-07 19:08:54 +0100

    Start energy price updates

---
+++ appdaemon/apps/energy_calc.py
@@ -0,0 +1,20 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+from datetime import timedelta, datetime, timezone
+
+class EnergyCalc(hass.Hass):
+
+    def initialize(self):
+        self.update()
+        self.run_daily(self.run_daily_c, "15:30:00")
+
+    def run_daily_c(self, **kwargs):
+        self.update()
+
+    def update(self):
+        present = datetime.now()
+        future = datetime(present.year, present.month, present.day, 12) + timedelta(1)
+        
+        self.log("Getting updated energy prices until %s" % future.isoformat())
+        resp = self.call_service("tibber.get_prices", end=future.isoformat())
+        self.log("Response %r" % resp)
