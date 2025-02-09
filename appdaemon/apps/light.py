e2694b665b960f30936c64f76bd5719801f59035
Fabian Jonassen <fabian.jonassen@paymenttools.com>
2025-02-09 22:51:37 +0100

    Debug

--- appdaemon/apps/light.py
+++ appdaemon/apps/light.py
@@ -253,7 +253,8 @@ class Light(hass.Hass):
             start_time =  now - timedelta(minutes = 30)
             data = self.get_history(entity_id = sensor, start_time = start_time)
             for d in data:
-                self.log(d[0])
+                for da in d:
+                    self.log(da)
 
         return rate / float(amount)
 
