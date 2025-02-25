dfe051c7a01b27ff018a990c620a778ab518d081
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2025-02-25 18:27:45 +0100

    Fix entity id

--- appdaemon/apps/energy_calc.py
+++ appdaemon/apps/energy_calc.py
@@ -2,7 +2,7 @@ import appdaemon.plugins.hass.hassapi as hass
 
 from datetime import timedelta, datetime, timezone
 
-EXPORT_SENSOR = "sensor.solaredge_exportierte_energies"
+EXPORT_SENSOR = "sensor.solaredge_exportierte_energie"
 
 class EnergyCalc(hass.Hass):
 
@@ -15,7 +15,7 @@ class EnergyCalc(hass.Hass):
 
     def update(self):
         now = datetime.now()
-       # current_value = float(self.get_state(EXPORT_SENSOR))
+        current_value = float(self.get_state(EXPORT_SENSOR))
         start_time =  now - timedelta(minutes = 5)
         data = self.get_history(entity_id = EXPORT_SENSOR, start_time = start_time)
         self.log(data)
