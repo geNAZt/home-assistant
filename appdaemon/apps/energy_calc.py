9fba7ff03e5a54f24351aac12d25b1b54b085519
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2025-02-25 20:52:26 +0100

    F

--- appdaemon/apps/energy_calc.py
+++ appdaemon/apps/energy_calc.py
@@ -37,12 +37,12 @@ class EnergyCalc(hass.Hass):
 
     def calc_wattage(self, entity, now):
         current_value = float(self.get_state(entity))
-        start_time =  now - timedelta(minutes = 10)
+        start_time =  now - timedelta(minutes = 15)
         data = self.get_history(entity_id = entity, start_time = start_time)
         history_state = float(data[0][0]['state'])
 
         if current_value >= history_state:
-            hourly = (current_value - history_state) * 6
+            hourly = (current_value - history_state) * 4
             if data[0][0]['attributes']['unit_of_measurement'] == 'kWh':
                 hourly *= 1000
 
