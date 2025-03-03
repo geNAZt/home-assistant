d3780a83089c07b5235c0cdd585d2fb7e0982b38
Fabian Jonassen <jonassen.fabian@gmail.com>
2025-03-04 00:28:33 +0100

    Update energy_manager.py

--- appdaemon/apps/energy_manager.py
+++ appdaemon/apps/energy_manager.py
@@ -242,6 +242,8 @@ class EnergyManager(hass.Hass):
             battery_charge_in_kwh = self._get_current_battery_capacity()
 
             time_sunrise = datetime.fromisoformat(self.get_state("sensor.sun_next_rising"))
+
+            self.lig("sunrise %s" % time_sunrise)
             time_till_sunrise = (stop_charging - time_sunrise).total_seconds()
 
             minutes, rest = divmod(time_till_sunrise, 60)
