54e6645e7245d3ad4e22614183caceaaba5195ee
Fabian Jonassen <fabian.jonassen@paymenttools.com>
2025-05-29 10:00:55 +0200

    Debug

--- appdaemon/apps/heating.py
+++ appdaemon/apps/heating.py
@@ -304,6 +304,7 @@ class Heating(hass.Hass):
             if len(data) > 0:
                 if len(data[0]) > 0:
                     try:
+                        print(data[0][0])
                         state = float(data[0][0]['state'])
                         date = datetime.fromisoformat(data[0][0]['last_changed'])
                         diffTime = now.astimezone(timezone.utc) - date
