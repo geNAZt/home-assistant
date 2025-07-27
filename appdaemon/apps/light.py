68ad465bf873ab31e3a4916f0e789c81a7137dc9
Fabian Jonassen <fabian.jonassen@paymenttools.com>
2025-07-27 11:41:54 +0200

    Only listen to call_service on own virtual entity name

--- appdaemon/apps/light.py
+++ appdaemon/apps/light.py
@@ -69,7 +69,7 @@ class Light(hass.Hass):
             "rgb_color": [round(r),round(g),round(b)]
         })
 
-        self.listen_event(self.onEvent, event="call_service")
+        self.listen_event(self.onEvent, event="call_service", entity_id=self.virtual_entity_name)
     
         # Attach a listener to all presence sensors
         self.presence_sensors = self.find_entity("binary_sensor.presence_%s[_0-9]*" % self.name.replace("light_", ""))
